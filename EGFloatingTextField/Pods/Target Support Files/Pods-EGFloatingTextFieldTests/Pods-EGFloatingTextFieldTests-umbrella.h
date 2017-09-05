cc461c89e2fe3c77d3735136704d9af5174a5481
Rimon Hanna <rimon.hanna@live.com>
2017-09-05 22:41:57 +0200

    pod install

--- EGFloatingTextField/Pods/Target Support Files/Pods-EGFloatingTextFieldTests/Pods-EGFloatingTextFieldTests-umbrella.h
+++ EGFloatingTextField/Pods/Target Support Files/Pods-EGFloatingTextFieldTests/Pods-EGFloatingTextFieldTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_EGFloatingTextFieldVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EGFloatingTextFieldVersionString[];
+FOUNDATION_EXPORT double Pods_EGFloatingTextFieldTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EGFloatingTextFieldTestsVersionString[];
 
